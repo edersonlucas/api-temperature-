2fad9591c78d04c70e43c540264727fedf7dc842
Ederson Lucas <edersonlucas@outlook.com.br>
2022-09-04 15:55:56 -0300

    feat: create the city endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const city = require('./Routes/cityRouter')
 
 const app = express();
 
 app.use(express.json());
+app.use('/city', city);
 
 module.exports = app;
